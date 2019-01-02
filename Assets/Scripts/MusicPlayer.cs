f971d77321640c84c692cba6031511a2ffa91b1c
Matthew Mok <matthewmoktf@gmail.com>
2019-01-02 17:47:54 +0800

    Manipulating environments and pathing
    Pathing using Unity standard asset scripts right now

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        Invoke("LoadNextScene", 2f);
+    }
+
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
