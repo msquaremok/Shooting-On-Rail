8a0bce67b2e6141ecb51fda8045a5618df7a58fc
Matthew Mok <matthewmoktf@gmail.com>
2019-01-04 18:21:39 +0800

    Minor updates for collision, score and FX

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+    [SerializeField] float destructDelay = 3f;
+
+	void Start ()
+    {
+        Destroy(gameObject, destructDelay);
+	}
+}
